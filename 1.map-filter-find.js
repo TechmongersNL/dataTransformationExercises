467c947a1d51e8a44a93654fb7222ffd0858d31e
reinoptland <reinoptland@gmail.com>
2019-05-01 11:23:38 +0200

    Add findPokemonById and test

--- 1.map-filter-find.js
+++ 1.map-filter-find.js
@@ -2,6 +2,12 @@ const getPokeNames = (pokemons) => {
     return pokemons.map(pokemon => pokemon.name)
 }
 
+const findPokemonById = (pokemons, id) => {
+    return pokemons.find(pokemon => pokemon.id === id)
+}
+
+
 module.exports = {
-    getPokeNames
+    getPokeNames,
+    findPokemonById
 }
--- 1.map-filter-find.test.js
+++ 1.map-filter-find.test.js
@@ -1,5 +1,5 @@
 const pokemons = require('./pokeData')
-const { getPokeNames } = require('./1.map-filter-find');
+const { getPokeNames, findPokemonById } = require('./1.map-filter-find');
 
 test('getPokeNames: Transforms an array of pokemons into an array of pokemon names', () => {
     const pokemonNames = getPokeNames(pokemons)
@@ -8,3 +8,9 @@ test('getPokeNames: Transforms an array of pokemons into an array of pokemon nam
     expect(pokemonNames[pokemonNames.length - 1]).toBe('Mew')
 });
 
+test('findPokemonById: Finds and returns a pokemon object by their id', () => {
+    const id = 25
+    const pokemon = findPokemonById(pokemons, id)
+    expect(pokemon.name).toBe('Pikachu')
+    expect(pokemon.height).toBe('0.41 m')
+})
