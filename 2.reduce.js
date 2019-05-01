114b639d513a3a57bfddaa7b433f54be790b36e0
reinoptland <reinoptland@gmail.com>
2019-05-01 15:21:03 +0200

    Add averageSpawnChance and totalEggDistance + tests

--- 2.reduce.js
+++ 2.reduce.js
@@ -4,6 +4,35 @@ const calculateTotalPokemonWeight = (pokemons) => {
     }, 0)
 }
 
+const calculateAverageSpawnChance = (pokemons) => {
+    const totalPokemonCount = pokemons.length
+    const averageSpawnChance = pokemons.reduce((totalSpawnChance, currentPokemon) => {
+        return totalSpawnChance + currentPokemon.spawn_chance
+    }, 0) / totalPokemonCount
+
+    return averageSpawnChance
+}
+
+const calculateTotalEggDistance = (pokemons) => {
+    return pokemons.reduce((totalDistance, currentPokemon) => {
+        if (currentPokemon.egg === "Not in Eggs") {
+            return totalDistance
+        }
+
+        return totalDistance + parseInt(currentPokemon.egg)
+    }, 0)
+}
+
+// Alternate solution using ternary operator
+// const calculateTotalEggDistance = (pokemons) => {
+//     return pokemons.reduce((totalDistance, currentPokemon) => {
+//         const distance = currentPokemon.egg === "Not in Eggs" ? 0 : parseInt(currentPokemon.egg)
+//         return totalDistance + distance
+//     }, 0)
+// }
+
 module.exports = {
-    calculateTotalPokemonWeight
+    calculateTotalPokemonWeight,
+    calculateAverageSpawnChance,
+    calculateTotalEggDistance
 }
--- 2.reduce.test.js
+++ 2.reduce.test.js
@@ -1,7 +1,17 @@
 const pokemons = require('./pokeData')
-const { calculateTotalPokemonWeight } = require('./2.reduce');
+const { calculateTotalPokemonWeight, calculateAverageSpawnChance, calculateTotalEggDistance } = require('./2.reduce');
 
 test('calculateTotalPokemonWeight: calculates the combined weight of all 151 pokemon', () => {
     const totalweight = calculateTotalPokemonWeight(pokemons)
     expect(totalweight).toBe(6938.7)
 });
+
+test('calculateAverageSpawnChance: calculates the average spawn_chance of a pokemon', () => {
+    const averageSpawnChance = calculateAverageSpawnChance(pokemons)
+    expect(averageSpawnChance).toBeCloseTo(0.73, 2)
+})
+
+test('calculateTotalEggDistance: calculates how for you have to walk to hatch one of each pokemon egg', () => {
+    const totalEggDistance = calculateTotalEggDistance(pokemons)
+    expect(totalEggDistance).toBe(408)
+})
