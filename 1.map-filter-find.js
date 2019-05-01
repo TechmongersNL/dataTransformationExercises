fc7bbd8198068d0512ee4f9ac2718583f247cdcf
reinoptland <reinoptland@gmail.com>
2019-05-01 23:37:21 +0200

    Remove solution code, add test scripts

--- 1.map-filter-find.js
+++ 1.map-filter-find.js
@@ -3,30 +3,10 @@ const getPokeNames = (pokemons) => {
 }
 
 const getPokemonById = (pokemons, id) => {
-    return pokemons.find(pokemon => pokemon.id === id)
-}
 
-const getRarePokemons = (pokemons) => {
-    return pokemons.filter(pokemon => pokemon.spawn_chance < 0.10)
-}
-
-const getMidSizedPokemon = (pokemons) => {
-    return pokemons.find(pokemon => pokemon.weight === "38.0 kg")
-}
-
-const getAdultPokemons = (pokemons) => {
-    return pokemons.filter(pokemon => pokemon.egg === "Not in Eggs")
-}
-
-const getPokemonImages = (pokemons) => {
-    return pokemons.map(pokemon => pokemon.img)
 }
 
 module.exports = {
     getPokeNames,
     getPokemonById,
-    getRarePokemons,
-    getMidSizedPokemon,
-    getAdultPokemons,
-    getPokemonImages
 }
