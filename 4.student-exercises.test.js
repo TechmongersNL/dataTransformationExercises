250ad519aa01831b93724ff6f5ac7386f063beec
Billy Vlachos <williamcaminiti@live.com>
2019-06-06 15:12:25 +0200

    Included additional expect to test for gym city names

--- 4.student-exercises.test.js
+++ 4.student-exercises.test.js
@@ -4,6 +4,11 @@ const gyms = require("./gymData");
 
 const { getPsychicTrainersAndGyms } = require("./4.student-exercises");
 
+/**
+ * This function should return an array of trainers that have
+ * at least one psychic pokemon. In each trainer object an array
+ * of their own pokemons and an object representing their gym should exist
+ */
 test(`getPsychicTrainersAndGyms: expect a list of trainers with psychic pokemons.
     also include (all) their pokemons and their gym`, () => {
   const result = getPsychicTrainersAndGyms(gyms, trainers, pokemons);
@@ -13,6 +18,8 @@ test(`getPsychicTrainersAndGyms: expect a list of trainers with psychic pokemons
   // Results should also contain gym
   expect(result[0].gym).toEqual(expect.any(Object));
   expect(result[1].gym).toEqual(expect.any(Object));
+  expect(result[0].gym.city).toBe("Saffron City");
+  expect(result[1].gym.city).toBe("Cerulean City");
   // Results should also contain array of pokemons
   expect(result[0].pokemons).toEqual(expect.any(Array));
   expect(result[1].pokemons).toEqual(expect.any(Array));
