769cd84c332f3602fefd477aa83db068133a9f72
Billy Vlachos <williamcaminiti@live.com>
2019-06-06 17:41:30 +0200

    Removed comments and solutions

--- 4.student-exercises.js
+++ 4.student-exercises.js
@@ -1,64 +1,13 @@
 const getTrainersAndGymsAndPokemons = (gyms, trainers, pokemons) => {
-  // Combine trainers and pokemons and gyms
-  return (
-    trainers
-      // Map trainers to include their pokemons
-      .map(trainer => {
-        // Include the trainer's pokemons
-        trainer.pokemons = pokemons.filter(pokemon =>
-          trainer.pokemonIds.includes(pokemon.id)
-        );
-        // Include the trainer's gym
-        trainer.gym = gyms.find(gym => gym.trainerId === trainer.id);
-        return trainer;
-      })
-  );
+
 };
 
 const getPsychicTrainersAndGyms = (gyms, trainers, pokemons) => {
-  return (
-    // Step 1: Combine the trainers, gyms and pokemons
-    getTrainersAndGymsAndPokemons(gyms, trainers, pokemons)
-      // Step 2: Filter out only trainers that have a psychic pokemon
-      .filter(trainer =>
-        trainer.pokemons.some(pokemon => pokemon.type.includes("Psychic"))
-      )
-  );
+
 };
 
 const getGymsWithPokemons = (gyms, trainers, pokemons, ...pokemonsToFind) => {
-  // Retrieve the pokemon objects
-  return (
-    pokemons
-      // Filter all the pokemons based on the ones we requested to find
-      .filter(pokemon => pokemonsToFind.includes(pokemon.name))
-      .map(pokemon => {
-        // Construct the object to be returned
-        return {
-          id: pokemon.id,
-          name: pokemon.name,
-          // Retrieve the trainers that own this pokemon
-          trainers: trainers.filter(trainer =>
-            trainer.pokemonIds.includes(pokemon.id)
-          ),
-          // Retrieve the gyms that are owned by these trainers
-          gyms: gyms.filter(gym => {
-            return trainers
-              .filter(trainer => trainer.pokemonIds.includes(pokemon.id))
-              .find(trainer => trainer.id === gym.trainerId);
-          })
-        };
-      })
-  );
 
-  return getTrainersAndGymsAndPokemons(gyms, trainers, pokemons)
-    .filter(trainer =>
-      trainer.pokemons.some(pokemon => pokemonsToFind.includes(pokemon.name))
-    )
-    .map(trainer => {
-      return {};
-      trainer.gym;
-    });
 };
 
 module.exports = {
--- 4.student-exercises.test.js
+++ 4.student-exercises.test.js
@@ -8,55 +8,37 @@ const {
   getGymsWithPokemons
 } = require("./4.student-exercises");
 
-/**
- * This function should return an array of trainer objects
- * that also include an array of pokemons and an object of gym
- */
 test("getTrainersAndGymsAndPokemons", () => {
-  // Get an array of trainers that contain their gyms and pokemons
   const trainersAndGymsAndPokemons = getTrainersAndGymsAndPokemons(
     gyms,
     trainers,
     pokemons
   );
   expect(trainersAndGymsAndPokemons.length).toBe(9);
-  // Expect each trainer to have a gym object
   expect(trainersAndGymsAndPokemons.map(trainer => trainer.gym)).toEqual(
     expect.any(Object)
   );
-  // Expect each trainer to have a pokemon array
   expect(trainersAndGymsAndPokemons.map(trainer => trainer.pokemons)).toEqual(
     expect.any(Array)
   );
 });
 
-/**
- * This function should return an array of trainers that have
- * at least one psychic pokemon. In each trainer object an array
- * of their own pokemons and an object representing their gym should exist
- */
 test(`getPsychicTrainersAndGyms: expect a list of trainers with psychic pokemons.
     also include (all) their pokemons and their gym`, () => {
   const result = getPsychicTrainersAndGyms(gyms, trainers, pokemons);
   expect(result.length).toEqual(2);
   expect(result[0].name).toBe("Sabrina");
   expect(result[1].name).toBe("Misty");
-  // Results should also contain gym
   expect(result[0].gym).toEqual(expect.any(Object));
   expect(result[1].gym).toEqual(expect.any(Object));
   expect(result[0].gym.city).toBe("Saffron City");
   expect(result[1].gym.city).toBe("Cerulean City");
-  // Results should also contain array of pokemons
   expect(result[0].pokemons).toEqual(expect.any(Array));
   expect(result[1].pokemons).toEqual(expect.any(Array));
   expect(result[0].pokemons.length).toBe(4);
   expect(result[1].pokemons.length).toBe(2);
 });
 
-/**
- * Returns an array of objects with the id and name of pokemon and an array
- * of gym objects where these pokemons can be found.
- */
 test("getGymsWithPokemons: expect a list of gyms where certain pokemons can be found", () => {
   const result = getGymsWithPokemons(
     gyms,
@@ -65,27 +47,22 @@ test("getGymsWithPokemons: expect a list of gyms where certain pokemons can be f
     "Gloom",
     "Starmie"
   );
-  // Expect the result to be an array
   result.forEach(pokemon => {
-    // It is an array of pokemons
     expect(pokemon).toEqual(
       expect.objectContaining({
         id: expect.any(Number),
         name: expect.any(String),
-        trainers: expect.any(Array), // Should contain a list of trainers
-        gyms: expect.any(Array) // Should contain a list of gyms
+        trainers: expect.any(Array),
+        gyms: expect.any(Array)
       })
     );
   });
-  // Length of array should be 2
   expect(result.length).toEqual(2);
-  // Check owners
   expect(result[0].trainers.length).toEqual(2);
   expect(result[1].trainers.length).toEqual(1);
   expect(result[0].trainers[0].name).toBe("Brock");
   expect(result[0].trainers[1].name).toBe("Erika");
   expect(result[1].trainers[0].name).toBe("Misty");
-  // Check Gyms
   expect(result[0].gyms.length).toEqual(2);
   expect(result[1].gyms.length).toEqual(1);
   expect(result[0].gyms[0].city).toBe("Celadon City");
