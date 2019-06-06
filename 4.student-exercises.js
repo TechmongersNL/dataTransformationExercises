0288db371b34afed397be226f987d7a75f390573
Billy Vlachos <williamcaminiti@live.com>
2019-06-06 16:13:04 +0200

    Adjusted getPsychicTrainersAndGyms function to use newly added function

--- 4.student-exercises.js
+++ 4.student-exercises.js
@@ -9,33 +9,21 @@ const getTrainersAndGymsAndPokemons = (gyms, trainers, pokemons) => {
           trainer.pokemonIds.includes(pokemon.id)
         );
         // Include the trainer's gym
-        trainer.gym = gyms.filter(gym => gym.trainerId === trainer.id);
+        trainer.gym = gyms.find(gym => gym.trainerId === trainer.id);
         return trainer;
       })
   );
 };
 
 const getPsychicTrainersAndGyms = (gyms, trainers, pokemons) => {
-  // Step 1: Combine trainers and psychic pokemons
-  let trainersWithPsychicPokemons = trainers
-    // Map trainers to include their pokemons
-    .map(trainer => {
-      trainer.pokemons = pokemons.filter(pokemon =>
-        trainer.pokemonIds.includes(pokemon.id)
-      );
-      return trainer;
-    })
-    // Filter the resulted trainer and return only the ones
-    // that actually have at least one psychic pokemon
-    .filter(trainer =>
-      trainer.pokemons.some(pokemon => pokemon.type.includes("Psychic"))
-    );
-  // Retrieve the gyms of these trainers
-  return trainersWithPsychicPokemons.map(trainer => {
-    // Assign the gym to the trainer
-    trainer.gym = gyms.find(gym => gym.trainerId === trainer.id);
-    return trainer;
-  });
+  return (
+    // Step 1: Combine the trainers, gyms and pokemons
+    getTrainersAndGymsAndPokemons(gyms, trainers, pokemons)
+      // Step 2: Filter out only trainers that have a psychic pokemon
+      .filter(trainer =>
+        trainer.pokemons.some(pokemon => pokemon.type.includes("Psychic"))
+      )
+  );
 };
 
 module.exports = {
